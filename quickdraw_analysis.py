72f4d1b1f935028e94d00e4e72549bcefe66da98
Alina Musteata <alinamusteata34@gmail.com>
2026-06-05 12:57:03 +0100

    Added data visualization chart

--- quickdraw_analysis.py
+++ quickdraw_analysis.py
@@ -1,10 +1,24 @@
-categories = ["cat", "dog", "car"]
+import matplotlib.pyplot as plt
 
-print("Quick Draw Dataset Exploration")
-print("-------------------------------")
+# Sample Quick Draw category counts
+drawing_counts = {
+    "Cat": 123202,
+    "Dog": 152159,
+    "Car": 98331,
+    "Cloud": 117587
+}
 
-print("Number of categories selected:", len(categories))
-print()
+categories = list(drawing_counts.keys())
+counts = list(drawing_counts.values())
 
-for category in categories:
-    print("Category:", category)
+plt.figure(figsize=(8, 5))
+plt.bar(categories, counts)
+
+plt.title("Quick Draw Dataset Categories")
+plt.xlabel("Category")
+plt.ylabel("Number of Drawings")
+
+plt.savefig("quickdraw_chart.png")
+plt.show()
+
+print("Chart saved as quickdraw_chart.png")
